72eb0b133cba3c5b9a1e73657995ef80f42f4ea2
Yago Arroyo <yagoag@gmail.com>
2015-07-26 14:08:53 -0700

    Better constructor and fixes to Transaction class

--- src/includes/classes/Transaction.php
+++ src/includes/classes/Transaction.php
@@ -1,7 +1,10 @@
 <?php
 namespace HouSplit;
+require_once 'Message.php';
+require_once 'Connection.php';
 
 class Transaction {
+    private $connection;
     private $type;
     private $description;
     private $value;
@@ -16,30 +19,15 @@ class Transaction {
      * @return bool
      */
     private function is_valid_type($type) {
-        return in_array($type, Payment::$types);
+        return in_array($type, Transaction::$types);
     }
 
     /**
-     * @param $type
-     * @param $description
-     * @param $value
-     * @param $payer
-     * @param $participants
+     * Add new transaction into database and
      */
-    public function __construct($type, $description, $value, $payer, $participants) {
-        if ($this->is_valid_type($type))
-            $this->type = $type;
-        $this->description = $description;
-        $this->value = str_replace(',', '.', $value);
-        $this->payer = $payer;
-        if (!in_array($payer, $participants))
-            $participants[] = $payer;
-        $this->participants = $participants;
-    }
-
-    public function add_to_database() {
-        $query = $this->connection->prepare('INSERT INTO transactions (name, payer, date, type, '.
-                                            'value) VALUES (?, ?, now(), ?, ?)');
+    private function add_to_database() {
+        $query = $this->connection->prepare('INSERT INTO transactions (name, payer, date, type, ' .
+            'value) VALUES (?, ?, now(), ?, ?)');
         $query->bind_param('sisd', $this->description, $this->payer, $this->type, $this->value);
         $query->execute();
 
@@ -51,11 +39,11 @@ class Transaction {
         else
             $portion_value = $this->value;
 
-        $new_portion = $this->connection->prepare('INSERT INTO portions (memberID, transactionID, '.
-                                                  'value) VALUES (?, ?, ?)');
+        $new_portion = $this->connection->prepare('INSERT INTO portions (memberID, transactionID, ' .
+            'value) VALUES (?, ?, ?)');
         $new_portion->bind_param('iid', $member, $transaction, $value);
-        $update_balance = $this->connection->prepare('UPDATE members SET balance = balance + ? '.
-                                                     'WHERE id = ?');
+        $update_balance = $this->connection->prepare('UPDATE members SET balance = balance + ? ' .
+            'WHERE id = ?');
         $update_balance->bind_param('di', $value, $member);
         foreach ($this->participants as $member) {
             if ($member == $this->payer)
@@ -66,8 +54,46 @@ class Transaction {
             $update_balance->execute();
         }
 
-        echo '<div class="title">' . $lang['success'] . '</div>';
-        echo $lang['msg_bill_added'];
+        $this->id = $transaction;
+
+        return new Message($lang['success'], $lang['msg_bill_added']);
+    }
+
+    private function get_from_database($id) {
+        throw new \ErrorException('Transaction.get_from_database() is not implemented yet.');
+    }
+
+    /**
+     * @param $type
+     * @param $description
+     * @param $value
+     * @param $payer
+     * @param $participants
+     */
+    public function __construct($connection = null, $id = null, $type = null, $description = null,
+                                $value = null, $payer = null, $participants = null) {
+        if ($id == null && $type = null)
+            throw new \ErrorException("Either the ID or all other fields of Transaction's " .
+                                      "construction should have a value.");
+
+        if ($connection)
+            $this->connection = $connection;
+        else
+            $this->connection = new Connection();
+
+        if ($id == null) {
+            if ($this->is_valid_type($type))
+                $this->type = $type;
+            $this->description = $description;
+            $this->value = str_replace(',', '.', $value);
+            $this->payer = $payer;
+            if (!in_array($payer, $participants))
+                $participants[] = $payer;
+            $this->participants = $participants;
+            $this->add_to_database();
+        } else {
+            $this->get_from_database($id);
+        }
     }
 }
 ?>
