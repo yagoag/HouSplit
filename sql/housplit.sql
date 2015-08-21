7d10950c2e50ee444a39b3154698084969d93d24
Yago Arroyo <yagoag@gmail.com>
2015-08-20 23:51:46 -0300

    Updated field types and sizes on table creation

--- sql/housplit.sql
+++ sql/housplit.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS members (
-    username TEXT NOT NULL,
-    name TEXT NOT NULL,
-    password TEXT NOT NULL,
-    salt TEXT NOT NULL,
-    active BOOL NOT NULL DEFAULT TRUE,
-    admin BOOL NOT NULL DEFAULT FALSE,
+    username VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    active TINYINT(1) NOT NULL DEFAULT TRUE,
+    admin TINYINT(1) NOT NULL DEFAULT FALSE,
     PRIMARY KEY (username)
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-    id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    name TEXT NOT NULL,
-    payer int NOT NULL,
+    id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    payer VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     type ENUM('Settle Up', 'Bill', 'Loan') NOT NULL,
     value FLOAT NOT NULL,
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS transactions (
 );
 
 CREATE TABLE IF NOT EXISTS portions (
-    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    member TEXT UNSIGNED NOT NULL,
-    transaction INT(10) UNSIGNED NOT NULL,
+    id INT(255) UNSIGNED NOT NULL AUTO_INCREMENT,
+    member VARCHAR(255) NOT NULL,
+    transaction INT(255) UNSIGNED NOT NULL,
     value INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (member)
