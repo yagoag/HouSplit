4e902f36846d59895eb731dc043478a23a4f6047
rcaporal <rafaelcaporal@gmail.com>
2017-06-26 19:42:32 +0000

    Fixed edit.php and register.php

--- src/admin/action/edit.php
+++ src/admin/action/edit.php
@@ -5,7 +5,9 @@
             $_POST['admin'] = 0;
         if (array_key_exists('deactivate', $_POST))
             $_POST['deactivate'] = 0;   // Note inverted value
-
+		else
+			$_POST['deactivate'] = 1;   // Note inverted value
+		
         if ($connection = new mysqli($mysql_server, $mysql_username, $mysql_password, $mysql_db)) {
             if ($connection->query('UPDATE members SET admin = '.$_POST['admin'].', active = '.$_POST['deactivate'].' WHERE id = '.$_POST['member'])) {
                 echo '<div class="title">' . $lang['success'] . '</div>';
--- src/admin/register.php
+++ src/admin/register.php
@@ -1,9 +1,9 @@
-<div class="title"><?php echo $lang['register_title']; ?></div>
+<div class="title"><?php echo $lang['register']; ?></div>
 <form id="register_form" name="register_form" method="post" action="?p=admin&act=register">
     <p><input type="text" size="35" name="name" placeholder="<?php echo $lang['name']; ?>" class="textbox" /></p>
     <p><input type="text" size="35" name="username" placeholder="<?php echo $lang['username']; ?>" class="textbox" /></p>
     <p><input type="password" size="35" name="password" placeholder="<?php echo $lang['password']; ?>" class="textbox" /></p>
     <p><input type="checkbox" name="admin" value="True"><?php echo $lang['member_is_admin']; ?></p>
     <br />
-    <p><input type="submit" name="register" value="<?php echo $lang['register_title']; ?>" class="button" /></p>
+    <p><input type="submit" name="register" value="<?php echo $lang['register']; ?>" class="button" /></p>
 </form>
